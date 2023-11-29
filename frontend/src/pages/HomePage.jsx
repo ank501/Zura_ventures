abfd716d0b10d0ba6b938fcc77c13fdb5dd38f29
ank501 <ankitsyadav246@gmail.com>
2023-11-30 02:48:42 +0530

    front page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import directright from "../images/directright.png"
 
 const HomePage = () => {
   return (
     <>
+    <div id='header'>
+      <div className="p-5 w-24 justify-evenly flex">
+        <img className='w-5 h-5' src={directright} alt="" srcset="" />
+        <div className=" text-5 text-purple-600  font-medium" >LAMA.</div>
+      </div>
+    </div>
     
     </>
   )
